4249015372f2214f210c55d55ef18a6e2a3b89a8
Enock <asanteenoch10@gmail.com>
2024-02-26 22:40:32 +0100

    update

--- mounty_main_fun.c
+++ mounty_main_fun.c
@@ -48,7 +48,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	else
 	{
 		parser->success = 0;
-		fprintf(stderr, "L%d : can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
